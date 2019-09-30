d76f14ef3825674a96f5b1a85d5c8c59c7857752
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-09-29 23:53:20 -0300

    Make comments

--- lib/diamond.rb
+++ lib/diamond.rb
@@ -1,22 +1,25 @@
 class Diamond
   def build(letter)
     letters = ('A'..letter).to_a
-    
     size = letters.length
-
     diamond = []
+    
+    # Build the upper side of diamond
     diamond << ('_' * (size - 1) + 'A' + '_' * (size - 1) + "\n")
     i = 1
     while i < size
       diamond << ('_' * (size - i - 1)) + letters[i] + ('_' * ((i * 2) - 1)) + letters[i] + ('_' * (size - i - 1)) + "\n"
       i += 1
     end
+
+    # Build the lower side of diamond
     i -= 2
     while i > 0
       diamond << ('_' * (size - i - 1)) + letters[i] + ('_' * ((i * 2) - 1)) + letters[i] + ('_' * (size - i - 1)) + "\n"
       i -= 1
     end
     diamond << '_' * (size - 1) + 'A' + '_' * (size - 1) + "\n" unless size == 1
+
     diamond.join('')
   end
 end
