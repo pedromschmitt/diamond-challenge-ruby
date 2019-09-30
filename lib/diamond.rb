b28232c2ca6e72c036c14ff38876adfacfdada0d
Pedro Schmitt <pedromartins.bsb@gmail.com>
2019-09-29 23:41:01 -0300

    remove unnecessary 'to_s" and rename diamond array

--- lib/diamond.rb
+++ lib/diamond.rb
@@ -1,22 +1,21 @@
 class Diamond
-
   def build(letter)
-    diamond_letters = ("A"..letter).to_a
-    size = diamond_letters.length.to_i
-    
-    d = []
-    d << ('_' * (size - 1) + 'A' + '_' * (size - 1) + "\n")
+    letters = ('A'..letter).to_a
+    size = letters.length.to_i
+
+    diamond = []
+    diamond << ('_' * (size - 1) + 'A' + '_' * (size - 1) + "\n")
     i = 1
     while i < size
-      d << ('_' * (size - i - 1)) + diamond_letters[i].to_s + ('_' * ((i * 2) - 1)) + diamond_letters[i].to_s + ('_' * (size - i - 1)) + "\n"
+      diamond << ('_' * (size - i - 1)) + letters[i] + ('_' * ((i * 2) - 1)) + letters[i] + ('_' * (size - i - 1)) + "\n"
       i += 1
     end
     i -= 2
     while i > 0
-      d << ('_' * (size - i - 1)) + diamond_letters[i].to_s + ('_' * ((i * 2) - 1)) + diamond_letters[i].to_s + ('_' * (size - i - 1)) + "\n"
+      diamond << ('_' * (size - i - 1)) + letters[i] + ('_' * ((i * 2) - 1)) + letters[i] + ('_' * (size - i - 1)) + "\n"
       i -= 1
     end
-    d << '_' * (size - 1) + 'A' + '_' * (size - 1) + "\n" unless size == 1
-    return d.join('')
+    diamond << '_' * (size - 1) + 'A' + '_' * (size - 1) + "\n" unless size == 1
+    diamond.join('')
   end
 end
